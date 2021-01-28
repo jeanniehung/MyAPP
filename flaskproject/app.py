09fc246d98a7d903d18f193e46e3b79babea990b
jeannie <18523883657@163.com>
2021-01-28 14:38:05 +0800

    x

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -131,9 +131,19 @@ def inject_user():  # 函数名可以随意修改
     return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
 
 
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('errors/400.html'), 400
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
